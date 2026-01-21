61ac3174f9dfa15ebba9fe9af1016c1803ec310a
sugwan <skhrnt2945@gmail.com>
2026-01-21 21:35:41 +0900

    init

---
+++ src/main/java/com/backend/onharu/OnharuApplication.java
@@ -0,0 +1,13 @@
+package com.backend.onharu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnharuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnharuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/onharu/OnharuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.onharu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnharuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
