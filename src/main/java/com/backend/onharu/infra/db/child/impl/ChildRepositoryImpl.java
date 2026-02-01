5e08ccff01390b11199c69018c56019f82c27192
jongwook.lee <net79736@gmail.com>
2026-02-02 01:03:07 +0900

    feat: Child 도메인 구현 - Child 엔티티 및 Repository 구현 - ChildCommandService, ChildQueryService 구현 - Child 관련 DTO (Command, Query, RepositoryParam)

--- src/main/java/com/backend/onharu/infra/db/child/impl/ChildRepositoryImpl.java
+++ src/main/java/com/backend/onharu/infra/db/child/impl/ChildRepositoryImpl.java
@@ -2,8 +2,12 @@ package com.backend.onharu.infra.db.child.impl;
 
 import org.springframework.stereotype.Repository;
 
+import com.backend.onharu.domain.child.dto.ChildRepositoryParam.GetChildByIdParam;
+import com.backend.onharu.domain.child.dto.ChildRepositoryParam.GetChildByLoginIdParam;
 import com.backend.onharu.domain.child.model.Child;
 import com.backend.onharu.domain.child.repository.ChildRepository;
+import com.backend.onharu.domain.support.error.CoreException;
+import com.backend.onharu.domain.support.error.ErrorType;
 import com.backend.onharu.infra.db.child.ChildJpaRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -20,5 +24,17 @@ public class ChildRepositoryImpl implements ChildRepository {
     @Override
     public Child save(Child child) {
         return childJpaRepository.save(child);
+    }
+
+    @Override
+    public Child getChildById(GetChildByIdParam param) {
+        return childJpaRepository.findById(param.id())
+                .orElseThrow(() -> new CoreException(ErrorType.Child.CHILD_NOT_FOUND));
+    }
+
+    @Override
+    public Child getChildByLoginId(GetChildByLoginIdParam param) {
+        return childJpaRepository.findByUser_LoginId(param.loginId())
+                .orElseThrow(() -> new CoreException(ErrorType.Child.CHILD_NOT_FOUND));
     }
 }
