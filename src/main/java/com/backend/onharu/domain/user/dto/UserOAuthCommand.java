925a8d071ddc894f3734e3e79121063711847071
sugwan <skhrnt2945@gmail.com>
2026-02-01 17:02:06 +0900

    feat: 소셜 사용자 Command DTO
    - 소셜 사용자 생성, 로그인, 회원가입 Command DTO 를 추가하였습니다.

---
+++ src/main/java/com/backend/onharu/domain/user/dto/UserOAuthCommand.java
@@ -0,0 +1,70 @@
+package com.backend.onharu.domain.user.dto;
+
+import com.backend.onharu.domain.common.enums.ProviderType;
+import com.backend.onharu.domain.user.model.User;
+
+/**
+ * 소셜 사용자 관련 Command DTO
+ */
+public class UserOAuthCommand {
+
+    /**
+     * 소셜 사용자 생성
+     *
+     * @param user         연동될 사용자
+     * @param providerType 소셜 사용자 타입
+     * @param providerId   소셜 사용자 ID
+     */
+    public record CreateUserOAuth(
+            User user,
+            ProviderType providerType,
+            String providerId
+    ) {
+    }
+
+    /**
+     * 소셜 사용자 로그인 Command
+     *
+     * @param loginId      로그인 아이디(소셜 이메일)
+     * @param name         이름
+     * @param phoneNumber  전화번호(미제공시 null 대신에 기본 문자열 삽입)
+     * @param providerType 소셜 로그인 타입
+     * @param providerId   소셜 로그인 고유 식별번호
+     */
+    public record LoginUserOAuthCommand(
+            String loginId,
+            String name,
+            String phoneNumber,
+            ProviderType providerType,
+            String providerId
+    ) {
+    }
+
+    /**
+     * 소셜 사용자(아동) 회원가입 Command
+     *
+     * @param userId      소셜 사용자 ID
+     * @param nickname    닉네임
+     * @param certificate 증명서 문서 경로
+     */
+    public record SignUpChildUserOAuthCommand(
+            String userId,
+            String nickname,
+            String certificate
+    ) {
+    }
+
+    /**
+     * 소셜 사용자(사업자) 회원가입 Command
+     *
+     * @param userId         소셜 사용자 ID
+     * @param businessNumber 사업자 등록번호
+     * @param levelId        등급 ID
+     */
+    public record SignUpOwnerUserOAuthCommand(
+            String userId,
+            String businessNumber,
+            String levelId
+    ) {
+    }
+}
