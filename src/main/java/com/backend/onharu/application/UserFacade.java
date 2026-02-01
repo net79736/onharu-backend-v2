0894125046b41719e42ebfa0252a1378a4b57870
sugwan <skhrnt2945@gmail.com>
2026-02-01 17:45:35 +0900

    fix: UserFacade 수정
    - 회원 조회를 위한 UserQueryService 의존성 추가
    - 소셜 로그인을 위한 Command/Query Service 의존성 추가
    - 비밀번호 암호화 검증을 위한 PasswordEncoder 의존성 추가
    - 사업자 회원가입시, 등급을 조회하는 로직 추가
    - 사용자 로그인 기능 추가
    - 소셜 로그인, 소셜 사용자 회원가입(아동, 사업자) 추가

--- src/main/java/com/backend/onharu/application/UserFacade.java
+++ src/main/java/com/backend/onharu/application/UserFacade.java
@@ -2,21 +2,41 @@ package com.backend.onharu.application;
 
 import com.backend.onharu.domain.child.dto.ChildCommand.CreateChildCommand;
 import com.backend.onharu.domain.child.service.ChildCommandService;
+import com.backend.onharu.domain.common.enums.StatusType;
+import com.backend.onharu.domain.common.enums.UserType;
 import com.backend.onharu.domain.level.dto.LevelQuery.GetLevelByIdQuery;
 import com.backend.onharu.domain.level.model.Level;
 import com.backend.onharu.domain.level.service.LevelQueryService;
 import com.backend.onharu.domain.owner.dto.OwnerCommand.CreateOwnerCommand;
 import com.backend.onharu.domain.owner.service.OwnerCommandService;
+import com.backend.onharu.domain.user.dto.UserCommand.CreateUserCommand;
+import com.backend.onharu.domain.user.dto.UserCommand.LoginUserCommand;
 import com.backend.onharu.domain.user.dto.UserCommand.SignUpChildCommand;
 import com.backend.onharu.domain.user.dto.UserCommand.SignUpOwnerCommand;
+import com.backend.onharu.domain.user.dto.UserOAuthCommand.CreateUserOAuth;
+import com.backend.onharu.domain.user.dto.UserOAuthCommand.LoginUserOAuthCommand;
+import com.backend.onharu.domain.user.dto.UserOAuthCommand.SignUpChildUserOAuthCommand;
+import com.backend.onharu.domain.user.dto.UserOAuthCommand.SignUpOwnerUserOAuthCommand;
+import com.backend.onharu.domain.user.dto.UserOAuthQuery.GetUserByUserOAuthQuery;
+import com.backend.onharu.domain.user.dto.UserQuery.GetUserByIdQuery;
+import com.backend.onharu.domain.user.dto.UserQuery.GetUserByLoginIdQuery;
 import com.backend.onharu.domain.user.model.User;
+import com.backend.onharu.domain.user.model.UserOAuth;
 import com.backend.onharu.domain.user.service.UserCommandService;
+import com.backend.onharu.domain.user.service.UserOAuthCommandService;
+import com.backend.onharu.domain.user.service.UserOAuthQueryService;
+import com.backend.onharu.domain.user.service.UserQueryService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 /**
  * 사용자 Facade
  */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class UserFacade {
@@ -25,6 +45,11 @@ public class UserFacade {
     private final OwnerCommandService ownerCommandService;
     private final ChildCommandService childCommandService;
     private final LevelQueryService levelQueryService;
+    private final UserQueryService userQueryService;
+    private final UserOAuthQueryService userOAuthQueryService;
+    private final UserOAuthCommandService userOAuthCommandService;
+
+    private final PasswordEncoder passwordEncoder;
 
     /**
      * 아동 회원가입을 처리합니다.
@@ -35,10 +60,9 @@ public class UserFacade {
      * @return 생성된 사용자 엔티티
      */
     public User signUpChild(SignUpChildCommand command) {
-        // 1. User 생성
+
         User user = userCommandService.signUpChild(command);
 
-        // 2. Child 생성 (User와 연결)
         CreateChildCommand createChildCommand = new CreateChildCommand(
                 user,
                 command.nickname(),
@@ -58,22 +82,149 @@ public class UserFacade {
      * @return 생성된 사용자 엔티티
      */
     public User signUpOwner(SignUpOwnerCommand command) {
-        // 1. User 생성
-        User user = userCommandService.signUpOwner(command);
 
-        // 2. Level 조회
         Level level = levelQueryService.getLevel(
                 new GetLevelByIdQuery(
                         Long.valueOf(command.levelId())
                 )
         );
 
-        // 3. Owner 생성 (User, Level 과 연결)
+        User user = userCommandService.signUpOwner(command);
+
         CreateOwnerCommand createOwnerCommand = new CreateOwnerCommand(
                 user,
                 level,
                 command.businessNumber()
         );
+
+        ownerCommandService.createOwner(createOwnerCommand);
+
+        return user;
+    }
+
+    /**
+     * 사용자 로그인을 처리합니다.
+     * <p>
+     * User 를 생성하고 비밀번호와 계정 상태를 검증합니다.
+     *
+     * @param command 로그인 요청 Command
+     * @return 로그인 사용자 엔티티
+     */
+    public User loginUser(LoginUserCommand command) {
+
+        User user = userQueryService.getUserByLoginId(
+                new GetUserByLoginIdQuery(command.loginId())
+        );
+
+        user.verifyPassword(command.password(), passwordEncoder);
+        user.verifyStatus();
+
+        return user;
+    }
+
+    /**
+     * 소셜 로그인을 처리합니다.
+     * <p>
+     * 계정이 없는 경우 소셜 회원가입을 진행하고, 계정이 있는 경우 소셜 계정과 연동한 뒤, 사용자 계정 상태를 검증합니다.
+     *
+     * @param command 소셜 로그인 요청
+     * @return 사용자 엔티티
+     */
+    public User loginUserOAuth(LoginUserOAuthCommand command) {
+
+        User user = userOAuthQueryService.getUserByUserOAuthQuery(
+                        new GetUserByUserOAuthQuery(
+                                command.providerType(),
+                                command.providerId()
+                        )
+                )
+                .map(UserOAuth::getUser)
+                .orElseGet(() -> {
+                    User tempUser = userCommandService.createUser(
+                            new CreateUserCommand(
+                                    command.loginId(),
+                                    passwordEncoder.encode(UUID.randomUUID().toString()),
+                                    command.name(),
+                                    command.phoneNumber(),
+                                    UserType.NONE,
+                                    StatusType.PENDING
+                            ));
+
+                    userOAuthCommandService.createUserOAuth(
+                            new CreateUserOAuth(
+                                    tempUser,
+                                    command.providerType(),
+                                    command.providerId()
+                            )
+                    );
+
+                    return tempUser;
+                });
+
+        user.verifyStatus();
+
+        return user;
+    }
+
+    /**
+     * 소셜 사용자 아동 회원가입을 처리합니다.
+     * <p>
+     * 추가 정보를 받아 소셜 사용자 아동의 회원가입을 마무리 합니다.
+     *
+     * @param command 아동 회원가입에 필요한 추가 정보
+     * @return 사용자 엔티티
+     */
+    public User completeSignUpChildUserOAuth(SignUpChildUserOAuthCommand command) {
+
+        User user = userQueryService.getUser(
+                new GetUserByIdQuery(
+                        Long.valueOf(command.userId())
+                )
+        );
+
+        user.changeUserTypeToChild();
+
+        CreateChildCommand createChildCommand = new CreateChildCommand(
+                user,
+                command.nickname(),
+                command.certificate()
+        );
+
+        childCommandService.createChild(createChildCommand);
+
+        return user;
+    }
+
+    /**
+     * 소셜 사용자 사업자 회원가입을 처리합니다.
+     * <p>
+     * 추가 정보를 받아 소셜 사용자 사업자의 회원가입을 마무리 합니다.
+     *
+     * @param command 사업자 회원가입에 필요한 추가 정보
+     * @return 사용자 엔티티
+     */
+    public User completeSignUpOwnerUserOAuth(SignUpOwnerUserOAuthCommand command) {
+
+        Level level = levelQueryService.getLevel(
+                new GetLevelByIdQuery(
+                        Long.valueOf(command.levelId())
+                )
+        );
+
+        User user = userQueryService.getUser(
+                new GetUserByIdQuery(
+                        Long.valueOf(command.userId())
+                )
+        );
+
+        user.changeUserTypeToOwner();
+
+        CreateOwnerCommand createOwnerCommand = new CreateOwnerCommand(
+                user,
+                level,
+                command.businessNumber()
+        );
+
         ownerCommandService.createOwner(createOwnerCommand);
 
         return user;
