58adcfc7f6a44aeba6aaef2957d44dc3f88d77ec
sugwan <skhrnt2945@gmail.com>
2026-02-04 13:41:44 +0900

    docs: BCryptConfig 주석 추가

--- src/main/java/com/backend/onharu/config/BCryptConfig.java
+++ src/main/java/com/backend/onharu/config/BCryptConfig.java
@@ -5,9 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * 비밀번호 암호화 설정 클래스
+ */
 @Configuration
 public class BCryptConfig {
 
+    /**
+     * PasswordEncoder Bean 등록(BCrypt)
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
