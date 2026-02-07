89092768438a81ecfc1fcd2a507127eb498946b7
sugwan <skhrnt2945@gmail.com>
2026-02-07 16:31:03 +0900

    refactor: 찜하기 등록, 찜목록 조회, 찜 취소 API 수정
    - 각 API 별 childFacade 를 호출해서 서비스를 처리하도록 수정했습니다.
    - 현재 세션에서 childId 를 가져올 수 없기 때문에 하드코딩했습니다.

--- src/main/java/com/backend/onharu/interfaces/api/controller/impl/FavoriteControllerImpl.java
+++ src/main/java/com/backend/onharu/interfaces/api/controller/impl/FavoriteControllerImpl.java
@@ -1,13 +1,22 @@
 package com.backend.onharu.interfaces.api.controller.impl;
 
+import com.backend.onharu.application.ChildFacade;
+import com.backend.onharu.domain.favorite.dto.FavoriteCommand.CreateFavoriteCommand;
+import com.backend.onharu.domain.favorite.dto.FavoriteCommand.DeleteFavoriteCommand;
+import com.backend.onharu.domain.favorite.dto.FavoriteQuery.FindFavoritesByChildIdQuery;
+import com.backend.onharu.domain.favorite.model.Favorite;
 import com.backend.onharu.interfaces.api.common.dto.ResponseDTO;
 import com.backend.onharu.interfaces.api.controller.IFavoriteController;
+import com.backend.onharu.interfaces.api.dto.FavoriteControllerDto.FavoriteResponse;
 import com.backend.onharu.interfaces.api.dto.FavoriteControllerDto.CreateFavoriteResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.backend.onharu.interfaces.api.dto.FavoriteControllerDto.GetMyFavoriteListResponse;
 
@@ -17,10 +26,12 @@ import static com.backend.onharu.interfaces.api.dto.FavoriteControllerDto.GetMyF
 @RequiredArgsConstructor
 public class FavoriteControllerImpl implements IFavoriteController {
 
+    private final ChildFacade childFacade;
+
     /**
      * 찜등록
      *
-     * POST /api/favorites/stores/{storeId}
+     * POST /favorites/stores/{storeId}
      * 특정 가게에 대한 찜을 등록합니다.
      *
      * @param storeId 찜하기 ID
@@ -31,14 +42,25 @@ public class FavoriteControllerImpl implements IFavoriteController {
     public ResponseEntity<ResponseDTO<CreateFavoriteResponse>> createFavorite(@PathVariable("storeId") Long storeId) {
         log.info("찜하기 등록 요청: storeId={}", storeId);
 
+        // TODO: 하드코딩 - 시큐리티 세션에서 현재 로그인한 사용자(아동) ID 가져오기
+        Long childId = 1L;
+
+        // 찜등록
+        Favorite favorite = childFacade.createFavorite(
+                new CreateFavoriteCommand(childId, storeId)
+        );
+
+        // 응답 생성
+        CreateFavoriteResponse response = new CreateFavoriteResponse(favorite.getId());
+
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(ResponseDTO.success(null));
+                .body(ResponseDTO.success(response));
     }
 
     /**
      * 내 찜목록 조회
      *
-     * GET /api/favorites
+     * GET /favorites
      * 내가 작성한 찜목록을 조회합니다.
      *
      * @return 내가 작성한 찜목록
@@ -48,14 +70,27 @@ public class FavoriteControllerImpl implements IFavoriteController {
     public ResponseEntity<ResponseDTO<GetMyFavoriteListResponse>> getMyFavorite() {
         log.info("찜목록 조회 요청");
 
+        // TODO: 하드코딩 - 시큐리티 세션에서 현재 로그인한 사용자(아동) ID 가져오기
+        Long childId = 1L;
+
+        // 내 찜목록 조회
+        List<Favorite> favorites = childFacade.getMyFavorites(
+                new FindFavoritesByChildIdQuery(childId)
+        );
+
+        // 응답 리스트 생성
+        List<FavoriteResponse> favoriteResponses = favorites.stream()
+                .map(FavoriteResponse::new)
+                .collect(Collectors.toList());
+
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ResponseDTO.success(null));
+                .body(ResponseDTO.success(new GetMyFavoriteListResponse(favoriteResponses)));
     }
 
     /**
      * 내가 등록한 찜하기 취소
      *
-     * DELETE /api/favorites/{favoriteId}
+     * DELETE /favorites/{favoriteId}
      * 특정 찜하기를 삭제(취소)합니다.
      *
      * @param favoriteId 찜하기 ID
@@ -65,6 +100,14 @@ public class FavoriteControllerImpl implements IFavoriteController {
     @DeleteMapping("/{favoriteId}")
     public ResponseEntity<ResponseDTO<Void>> deleteFavorite(@PathVariable("favoriteId") Long favoriteId) {
         log.info("찜취소 요청: favoriteId={}", favoriteId);
+
+        // TODO: 하드코딩 - 시큐리티 세션에서 현재 로그인한 사용자(아동) ID 가져오기
+        Long childId = 1L;
+
+        // 찜하기 취소
+        childFacade.deleteFavorite(
+                new DeleteFavoriteCommand(childId, favoriteId)
+        );
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ResponseDTO.success(null));
