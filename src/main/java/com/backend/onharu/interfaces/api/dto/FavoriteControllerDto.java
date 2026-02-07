9948c78cb15dce350e16470f0fb25ec5c60ac242
sugwan <skhrnt2945@gmail.com>
2026-02-07 16:29:13 +0900

    refactor: 찜하기 컨트롤러 DTO 수정
    - 찜하기 응답 추가(찜하기 ID, 아동 ID, 가게 ID 반환)
    - 찜 취소 요청 추가

--- src/main/java/com/backend/onharu/interfaces/api/dto/FavoriteControllerDto.java
+++ src/main/java/com/backend/onharu/interfaces/api/dto/FavoriteControllerDto.java
@@ -1,18 +1,25 @@
 package com.backend.onharu.interfaces.api.dto;
 
+import com.backend.onharu.domain.favorite.model.Favorite;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
+/**
+ * 찜하기 API 에서 사용되는 DTO 입니다.
+ */
 public class FavoriteControllerDto {
 
     public record CreateFavoriteResponse(
+            @NotNull(message = "찜하기 ID 는 필수 입력 값 입니다.")
             @Schema(description = "찜하기 ID", example = "1")
             Long favoriteId
     ) {
     }
 
     public record GetMyFavoriteListResponse(
+            @Schema(description = "찜하기 목록")
             List<FavoriteResponse> favorites
     ) {
     }
@@ -26,6 +33,25 @@ public class FavoriteControllerDto {
 
             @Schema(description = "가게 ID", example = "200")
             Long storeId
+    ) {
+        public FavoriteResponse(Favorite favorite) {
+            this(
+                    favorite.getId(),
+                    favorite.getChild().getId(),
+                    favorite.getStore().getId()
+            );
+        }
+    }
+
+    @Schema(description = "찜하기 취소(삭졔) 요청")
+    public record DeleteFavoriteRequest(
+            @NotNull(message = "찜하기 ID 는 필수 입력 값 입니다.")
+            @Schema(description = "찜하기 ID", example = "1")
+            Long id,
+
+            @NotNull(message = "아이 ID 는 필수 입력 값 입니다.")
+            @Schema(description = "아이 ID", example = "10")
+            Long childId
     ) {
     }
 }
