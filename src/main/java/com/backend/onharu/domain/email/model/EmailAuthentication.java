06ea297a4e0b3e9bd3baeee9b8d0f69e143afe3f
sugwan <skhrnt2945@gmail.com>
2026-02-10 22:44:40 +0900

    refactor: 이메일 인증 엔티티에서 사용하지 않는 메서드 제거

--- src/main/java/com/backend/onharu/domain/email/model/EmailAuthentication.java
+++ src/main/java/com/backend/onharu/domain/email/model/EmailAuthentication.java
@@ -63,15 +63,6 @@ public class EmailAuthentication extends BaseEntity {
     }
 
     /**
-     * 이메일 인증 토큰이 만료되었는지 확인합니다.
-     *
-     * @param now 현재 시각
-     */
-    public boolean isExpired(LocalDateTime now) {
-        return expiredAt.isBefore(now);
-    }
-
-    /**
      * 이메일 인증을 검증합니다.
      *
      * @param now 현재 시각
@@ -86,14 +77,5 @@ public class EmailAuthentication extends BaseEntity {
         }
 
         this.isVerified = true;
-    }
-
-    /**
-     * 이메일이 검증되었는지 확인합니다.
-     */
-    public void check() {
-        if (!this.isVerified) { // 검증되지 않을 경우
-            throw new CoreException(EMAIL_NOT_VERIFIED);
-        }
     }
 }
