377cc8adf73d992ebc3bb0adbdceaff1eefcee5d
sugwan <skhrnt2945@gmail.com>
2026-02-19 16:31:06 +0900

    feat: UserFacade 에 아동/사업자 프로필 수정 기능 추가

--- src/main/java/com/backend/onharu/application/UserFacade.java
+++ src/main/java/com/backend/onharu/application/UserFacade.java
@@ -1,7 +1,6 @@
 package com.backend.onharu.application;
 
 import com.backend.onharu.domain.child.dto.ChildCommand.CreateChildCommand;
-import com.backend.onharu.domain.child.dto.ChildQuery;
 import com.backend.onharu.domain.child.model.Child;
 import com.backend.onharu.domain.child.service.ChildCommandService;
 import com.backend.onharu.domain.child.service.ChildQueryService;
@@ -9,24 +8,19 @@ import com.backend.onharu.domain.common.enums.StatusType;
 import com.backend.onharu.domain.common.enums.UserType;
 import com.backend.onharu.domain.level.dto.LevelQuery.GetLevelByIdQuery;
 import com.backend.onharu.domain.level.model.Level;
+import com.backend.onharu.domain.level.service.LevelCommandService;
 import com.backend.onharu.domain.level.service.LevelQueryService;
 import com.backend.onharu.domain.owner.dto.OwnerCommand.CreateOwnerCommand;
-import com.backend.onharu.domain.owner.dto.OwnerQuery;
 import com.backend.onharu.domain.owner.model.Owner;
 import com.backend.onharu.domain.owner.service.OwnerCommandService;
 import com.backend.onharu.domain.owner.service.OwnerQueryService;
-import com.backend.onharu.domain.user.dto.UserProfile;
-import com.backend.onharu.domain.user.dto.UserProfile.UserChildProfile;
-import com.backend.onharu.domain.user.dto.UserCommand.CreateUserCommand;
-import com.backend.onharu.domain.user.dto.UserCommand.LoginUserCommand;
-import com.backend.onharu.domain.user.dto.UserCommand.SignUpChildCommand;
-import com.backend.onharu.domain.user.dto.UserCommand.SignUpOwnerCommand;
+import com.backend.onharu.domain.user.dto.UserCommand.*;
 import com.backend.onharu.domain.user.dto.UserOAuthCommand.CreateUserOAuth;
 import com.backend.onharu.domain.user.dto.UserOAuthCommand.LoginUserOAuthCommand;
 import com.backend.onharu.domain.user.dto.UserOAuthCommand.SignUpChildUserOAuthCommand;
 import com.backend.onharu.domain.user.dto.UserOAuthCommand.SignUpOwnerUserOAuthCommand;
 import com.backend.onharu.domain.user.dto.UserOAuthQuery.GetUserByUserOAuthQuery;
-import com.backend.onharu.domain.user.dto.UserQuery;
+import com.backend.onharu.domain.user.dto.UserProfile.UserChildProfile;
 import com.backend.onharu.domain.user.dto.UserQuery.GetUserByIdQuery;
 import com.backend.onharu.domain.user.dto.UserQuery.GetUserByLoginIdQuery;
 import com.backend.onharu.domain.user.model.User;
@@ -42,10 +36,14 @@ import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
-import static com.backend.onharu.domain.child.dto.ChildQuery.*;
-import static com.backend.onharu.domain.owner.dto.OwnerQuery.*;
-import static com.backend.onharu.domain.user.dto.UserProfile.*;
-import static com.backend.onharu.domain.user.dto.UserQuery.*;
+import static com.backend.onharu.domain.child.dto.ChildCommand.UpdateChildCommand;
+import static com.backend.onharu.domain.child.dto.ChildQuery.GetChildByIdQuery;
+import static com.backend.onharu.domain.level.dto.LevelCommand.UpdateNameByIdCommand;
+import static com.backend.onharu.domain.owner.dto.OwnerCommand.updateOwnerBusinessNumberByIdCommand;
+import static com.backend.onharu.domain.owner.dto.OwnerQuery.GetOwnerByIdQuery;
+import static com.backend.onharu.domain.user.dto.UserProfile.UserOwnerProfile;
+import static com.backend.onharu.domain.user.dto.UserQuery.GetChildProfileQuery;
+import static com.backend.onharu.domain.user.dto.UserQuery.GetOwnerProfileQuery;
 
 /**
  * 사용자 Facade
@@ -66,6 +64,7 @@ public class UserFacade {
     private final UserOAuthCommandService userOAuthCommandService;
 
     private final PasswordEncoder passwordEncoder;
+    private final LevelCommandService levelCommandService;
 
     /**
      * 아동 회원가입을 처리합니다.
@@ -284,10 +283,10 @@ public class UserFacade {
     }
 
     /**
-     * 사용자(가게) 프로필 조회
+     * 사용자(사업자) 프로필 조회
      *
-     * @param query 사용자 ID, 등급 ID, 가게 ID 가 포함된 query
-     * @return 조회된 사용자, 등급, 가게 엔티티
+     * @param query 사용자 ID, 등급 ID, 사업자 ID 가 포함된 query
+     * @return 조회된 사용자, 등급, 사업자 엔티티
      */
     public UserOwnerProfile getOwnerProfile(GetOwnerProfileQuery query) {
         // 사용자 조회
@@ -297,19 +296,79 @@ public class UserFacade {
 
         user.verifyStatus(); // 계정 상태 확인
 
-        // 가게 조회
+        // 사업자 조회
         Owner owner = ownerQueryService.getOwnerById(
                 new GetOwnerByIdQuery(query.ownerId())
         );
 
-        Level level = owner.getLevel(); // 가게에 연결된 등급 도메인 추출
+        Level level = owner.getLevel(); // 사업자에 연결된 등급 도메인 추출
 
         return new UserOwnerProfile(user, level, owner);
     }
 
-    // 프로필 수정(아동)
+    /**
+     * 사용자(아동) 프로필 수정
+     *
+     * @param command 사용자 ID, 아동 ID, 이름, 전화번호, 닉네임이 포함된 command
+     */
+    public void updateChildProfile(UpdateChildProfileCommand command) {
+        // 사용자 조회
+        User user = userQueryService.getUser(
+                new GetUserByIdQuery(command.userId())
+        );
+        user.verifyStatus(); // 계정 상태 확인
 
-    // 프로필 수정(가게)
+        // 사용자 정보 업데이트
+        userCommandService.updateUserByIdAndNameAndPhone(
+                new UpdateUserCommand(
+                        command.userId(),
+                        user.getName(),
+                        user.getPhone()
+                )
+        );
+
+        // 아동 정보 업데이트
+        childCommandService.updateChildByNickname(
+                new UpdateChildCommand(
+                        command.childId(),
+                        command.nickname()
+                )
+        );
+    }
+
+    // 사용자(사업자) 프로필 수정
+    public void updateOwnerProfile(UpdateOwnerProfileCommand command) {
+        // 사용자 조회
+        User user = userQueryService.getUser(
+                new GetUserByIdQuery(command.userId())
+        );
+        user.verifyStatus(); // 계정 상태 확인
+
+        // 사용자 정보 업데이트
+        userCommandService.updateUserByIdAndNameAndPhone(
+                new UpdateUserCommand(
+                        command.userId(),
+                        user.getName(),
+                        user.getPhone()
+                )
+        );
+
+        // 등급 정보 업데이트
+        levelCommandService.updateNameById(
+                new UpdateNameByIdCommand(
+                        command.name(),
+                        command.levelId()
+                )
+        );
+
+        // 사용자(사업자) 정보 업데이트
+        ownerCommandService.updateOwnerBusinessNumberById(
+                new updateOwnerBusinessNumberByIdCommand(
+                        command.ownerId(),
+                        command.businessNumber()
+                )
+        );
+    }
 
     // 회원탈퇴
     public void deleteUser() {
