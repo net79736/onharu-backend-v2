9728d5d8cbab2bef24d4bf727a7d3aedff7003b3
thr13 <skhrnt2945@gmail.com>
2026-02-27 19:09:43 +0900

    feat: 아동 업데이트 Command DTO/Service 추가

--- src/main/java/com/backend/onharu/domain/child/service/ChildCommandService.java
+++ src/main/java/com/backend/onharu/domain/child/service/ChildCommandService.java
@@ -1,11 +1,14 @@
 package com.backend.onharu.domain.child.service;
 
+import com.backend.onharu.domain.child.dto.ChildCommand;
 import com.backend.onharu.domain.child.dto.ChildCommand.CreateChildCommand;
 import com.backend.onharu.domain.child.model.Child;
 import com.backend.onharu.domain.child.repository.ChildRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.backend.onharu.domain.child.dto.ChildCommand.*;
 
 /**
  * 아동 Command Service
@@ -31,5 +34,14 @@ public class ChildCommandService {
                 .build();
 
         return childRepository.save(child);
+    }
+
+    /**
+     * 아동 도메인의 변경사항을 DB 에 반영합니다.
+     *
+     * @param command 아동 도메인이 포함된 Command
+     */
+    public void updateChild(UpdateChildCommand command) {
+        childRepository.save(command.child());
     }
 }
