d812234d226da2998ed6584a191d2fd2470438b9
sugwan <skhrnt2945@gmail.com>
2026-03-16 21:21:23 +0900

    fix: 이메일 인증 생성 수정
    - 이메일 인증 조회를 하지 않고, 이메일 인증이 있다면 인증 요청 횟수를 조회한 후 1분내에 10번 이상 요청이 올 경우 예외를 발생하도록 수정하였습니다.
    - 또, 사용하던 이메일로 새로운 이메일 인증을 보낼 경우 새 인증 객체를 생성하도록 변경하였습니다.

--- src/main/java/com/backend/onharu/domain/email/service/EmailAuthenticationCommandService.java
+++ src/main/java/com/backend/onharu/domain/email/service/EmailAuthenticationCommandService.java
@@ -2,8 +2,6 @@ package com.backend.onharu.domain.email.service;
 
 import com.backend.onharu.domain.email.dto.EmailAuthenticationCommand.CompleteEmailAuthenticationCommand;
 import com.backend.onharu.domain.email.dto.EmailAuthenticationCommand.CreateEmailAuthenticationCommand;
-import com.backend.onharu.domain.email.dto.EmailAuthenticationRepositoryParam.FindByEmailAndTokenParam;
-import com.backend.onharu.domain.email.dto.EmailAuthenticationRepositoryParam.FindByEmailParam;
 import com.backend.onharu.domain.email.model.EmailAuthentication;
 import com.backend.onharu.domain.email.repository.EmailAuthenticationRepository;
 import com.backend.onharu.domain.support.error.CoreException;
@@ -11,7 +9,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static com.backend.onharu.domain.support.error.ErrorType.EmailAuthentication.EMAIL_ALREADY_VERIFIED;
+import java.time.LocalDateTime;
+
+import static com.backend.onharu.domain.email.dto.EmailAuthenticationRepositoryParam.*;
+import static com.backend.onharu.domain.support.error.ErrorType.EmailAuthentication.EMAIL_AUTHENTICATION_TOO_MANY_REQUEST;
 
 /**
  * 이메일 인증 Command Service
@@ -23,27 +24,35 @@ public class EmailAuthenticationCommandService {
 
     private final EmailAuthenticationRepository emailAuthenticationRepository;
 
-
     /**
-     * 이메일 인증을 생성하거나, 재인증을 처리합니다.
+     * 이메일 인증을 생성
      */
     public EmailAuthentication create(CreateEmailAuthenticationCommand command) {
-        // 기존 인증이 있는지 조회
-        EmailAuthentication emailAuthentication = emailAuthenticationRepository.findEmailAuthenticationByEmail(
-                new FindByEmailParam(command.email())
-        ).map(existing -> {
-            if (existing.isVerified()) { // 이미 검증 완료된 이메일 인증 정보가 존재하는 경우
-                throw new CoreException(EMAIL_ALREADY_VERIFIED);
-            }
-            // 아직 검증이 완료되지 않은 경우 인증 재발급
-            existing.reissue(command.expiredAt());
+        String email = command.email();
+        LocalDateTime expiredAt = command.expiredAt();
+        LocalDateTime now = LocalDateTime.now();
 
-            return existing;
-        }).orElseGet(() -> EmailAuthentication.builder()
-                .email(command.email())
-                .expiredAt(command.expiredAt())
-                .build()
+        // 이메일 인증 요청 횟수 조회
+        long countEmailAuthentication = emailAuthenticationRepository.countEmailAuthentication(
+                new CountEmailAuthenticationParam(email, now.minusMinutes(1)) // 1분을 기준으로 카운트
         );
+
+        // 1분 내 요청이 10번 이상일 경우 예외 발생
+        if (countEmailAuthentication >= 10) {
+            throw new CoreException(EMAIL_AUTHENTICATION_TOO_MANY_REQUEST);
+        }
+
+        // 기존의 이메일 인증 토큰 만료 처리
+        emailAuthenticationRepository.expireTokens(
+                new ExpireTokensParam(email, now)
+        );
+
+        // 새 이메일 인증 생성
+        EmailAuthentication emailAuthentication = EmailAuthentication.builder()
+                .email(email)
+                .expiredAt(expiredAt)
+                .build();
+
         // 이메일 인증 저장
         return emailAuthenticationRepository.save(emailAuthentication);
     }
@@ -52,12 +61,12 @@ public class EmailAuthenticationCommandService {
      * 이메일 인증 완료 처리
      */
     public void verify(CompleteEmailAuthenticationCommand command) {
-        // 이메일과 토큰으로 이메일 인증 엔티티 조회
-        EmailAuthentication emailAuthentication = emailAuthenticationRepository.findEmailAuthenticationByEmailAndToken(
-                new FindByEmailAndTokenParam(command.email(), command.token())
+        // 이메일로 마지막에 생성된 이메일 인증 엔티티 조회
+        EmailAuthentication emailAuthentication = emailAuthenticationRepository.findTopByEmailOrderByCreatedAtDesc(
+                new FindByEmailParam(command.email())
         );
 
-        // 이메일 인증 검증
+        // 토큰과 현재시각을 기준으로 이메일 인증 검증 (검증성공시 isVerified 를 true 로 변경)
         emailAuthentication.verify(command.token(), command.now());
 
         // isVerified = true 로 변경한 이메일 인증 엔티티 저장
