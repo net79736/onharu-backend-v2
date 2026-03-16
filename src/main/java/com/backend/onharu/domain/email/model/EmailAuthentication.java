dd3efb90980163dbb2910033fa7d6a42ac78eb1c
thr13 <skhrnt2945@gmail.com>
2026-03-16 09:32:00 +0900

    refactor: 이메일 토큰을 UUID 에서 숫자 6자리로 변경
    - 요청사항대로 인증코드(토큰)을 랜덤 UUID 로 생성하는 부분을 숫자 6자리를 생성하도록 변경

--- src/main/java/com/backend/onharu/domain/email/model/EmailAuthentication.java
+++ src/main/java/com/backend/onharu/domain/email/model/EmailAuthentication.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
+import java.util.Random;
 
 import static com.backend.onharu.domain.support.error.ErrorType.EmailAuthentication.*;
 
@@ -35,7 +35,7 @@ public class EmailAuthentication extends BaseEntity {
     private String email; // 인증 이메일
 
     @Column(name = "TOKEN", nullable = false, length = 36)
-    private String token; // 인증 토큰(UUID) -> 향후 이메일 + 토큰 으로 조회 예정
+    private String token; // 랜덤 숫자 6자리를 입력받도록 변경
 
     @Column(name = "IS_VERIFIED")
     private boolean isVerified; // 인증 여부
@@ -46,7 +46,7 @@ public class EmailAuthentication extends BaseEntity {
     @Builder
     public EmailAuthentication(String email, LocalDateTime expiredAt) {
         this.email = email;
-        this.token = UUID.randomUUID().toString(); // 인증 토큰은 UUID 로 생성
+        this.token = createToken(); // 랜덤 숫자 6자리 토큰
         this.isVerified = false; // 객체 생성시 미인증 상태
         this.expiredAt = expiredAt;
     }
@@ -57,17 +57,17 @@ public class EmailAuthentication extends BaseEntity {
      * @param expiredAt 만료 시각
      */
     public void reissue(LocalDateTime expiredAt) {
-        this.token = UUID.randomUUID().toString();
+        this.token = createToken(); // 랜덤 숫자 6자리 토큰
         this.isVerified = false;
         this.expiredAt = expiredAt;
     }
 
     /**
      * 이메일 인증을 검증합니다.
-     *
+     * @param token 인증 토큰
      * @param now 현재 시각
      */
-    public void verify(LocalDateTime now) {
+    public void verify(String token, LocalDateTime now) {
         if (this.isVerified) { // 이미 검증처리가 된 경우
             throw new CoreException(EMAIL_ALREADY_VERIFIED);
         }
@@ -76,6 +76,28 @@ public class EmailAuthentication extends BaseEntity {
             throw new CoreException(EMAIL_TOKEN_EXPIRED);
         }
 
+        if (!this.token.equals(token)) { // 입력된 토큰이 인증 토큰과 일치하지 않는 경우
+            throw new CoreException(EMAIL_TOKEN_MISMATCH);
+        }
+
         this.isVerified = true;
+    }
+
+    /**
+     * 인증 토큰용 랜덤 숫자 6자리 생성 메소드 입니다.
+     * @return 문자열 타입의 6자리 인증코드
+     */
+    private String createToken() {
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+        int num = 0;
+
+        // 각 자리별로 랜덤 숫자 생성
+        for (int i=0; i<6; i++) {
+            num = random.nextInt(10); // 0 부터 9까지 난수 생성
+            sb.append(num);
+        }
+
+        return sb.toString(); // 숫자를 문자열으로 반환
     }
 }
