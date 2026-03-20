b4812b7ddec4e5e311fcb67238981a5465af3fa5
sugwan <skhrnt2945@gmail.com>
2026-03-20 16:10:11 +0900

    docs: 메시지 전송 SockJS 주석처리

--- src/main/java/com/backend/onharu/config/WebSocketConfiguration.java
+++ src/main/java/com/backend/onharu/config/WebSocketConfiguration.java
@@ -15,14 +15,14 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat") // ws://서버주소//ws-chat
                 .setAllowedOriginPatterns("*") // TODO: 운영 서버 배포시 * 대신 프론트엔드 도메인 적기
-                .withSockJS()
+//                .withSockJS()
         ;
     }
 
     // prefix 설정
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic"); // 내장 메시지 브로커 -> 단체 채팅 /topic, 일대일 채팅 /queue
+        registry.enableSimpleBroker("/topic", "/queue"); // 내장 메시지 브로커 -> 단체 채팅 /topic, 일대일 채팅 /queue
         registry.setApplicationDestinationPrefixes("/app"); // 클라이언트 에서 서버로 메시지 보낼 때 붙이는 경로 -> 스프링이 @MessageMapping 가 붙은 컨트롤러로 전달
     }
 }
