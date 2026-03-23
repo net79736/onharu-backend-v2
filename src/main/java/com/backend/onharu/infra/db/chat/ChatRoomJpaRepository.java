c863390aef30ee47bb04118c49e7d439bcf9b4fd
sugwan <skhrnt2945@gmail.com>
2026-03-23 20:36:07 +0900

    docs: 채팅방 Repository 주석 및 Param 을 import 로 변경

--- src/main/java/com/backend/onharu/infra/db/chat/ChatRoomJpaRepository.java
+++ src/main/java/com/backend/onharu/infra/db/chat/ChatRoomJpaRepository.java
@@ -3,5 +3,8 @@ package com.backend.onharu.infra.db.chat;
 import com.backend.onharu.domain.chat.model.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * 채팅방 JPA 구현체
+ */
 public interface ChatRoomJpaRepository extends JpaRepository<ChatRoom, Long> {
 }
