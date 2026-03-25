8d5a17aa5bb8acbb05f11ac6bc04fe63629362ab
jongwook.lee <net79736@gmail.com>
2026-03-25 18:50:16 +0900

    feat: 현재 시간 기반의 예약 가능성 검증 로직 추가 - `OwnerFacade` 클래스에 현재 시간을 기준으로 예약 가능 여부를 검증하는 로직 추가. - `ErrorType`에 예약 시간이 만료된 경우의 예외 처리 추가. - 관련 테스트 케이스를 통해 예약 생성 시 유효성 검증 강화.

--- src/main/java/com/backend/onharu/OnharuApplication.java
+++ src/main/java/com/backend/onharu/OnharuApplication.java
@@ -1,5 +1,8 @@
 package com.backend.onharu;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OnharuApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnharuApplication.class, args);
+        SpringApplication.run(OnharuApplication.class, args);
+        
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+        System.out.println("Time (Option Fixed - Seoul): " + now);
+        LocalDateTime now2 = LocalDateTime.now();
+        System.out.println("Time (Default): " + now2);
 	}
 }
